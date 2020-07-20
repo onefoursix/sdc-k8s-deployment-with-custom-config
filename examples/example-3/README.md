74103706819f920725bded600109c37a3ac7b14a
Mark Brooks <mark@streamsets.com>
2020-07-20 12:31:47 -0700

    updates

--- examples/example-3/README.md
+++ examples/example-3/README.md
@@ -1,4 +1,6 @@
 ### Loading stage libs or other resources from a read-only Volume
 
-This example showing how to load stage libs, shared files and other resources from a read-only Volume.  This technique can be used to load any resources needed by SDC at deployment time that are not baked into the SDC image, including, stage libs, hadoop config files, lookup files, JDBC drivers, etc... 
+This example shows how to load stage libs, config files or other resources from a shared and presumably read-only Volume.  This technique can be used to load any resources needed by SDC at deployment time that are not baked into the SDC image, including stage libs, hadoop config files, lookup files, JDBC drivers, etc... 
+
+It is up to the deployer to populate the shared volume; the [get-stage-libs.sh]()
 
--- examples/example-3/get-stage-libs.sh
+++ examples/example-3/get-stage-libs.sh
@@ -7,8 +7,10 @@ SDC_VERSION=3.16.1
 
 BASE_URL=https://archives.streamsets.com/datacollector
 
+# A space separated list of stage libs to download
 SDC_STAGE_LIBS="streamsets-datacollector-aws-lib streamsets-datacollector-basic-lib streamsets-datacollector-bigtable-lib streamsets-datacollector-dataformats-lib streamsets-datacollector-dev-lib streamsets-datacollector-google-cloud-lib streamsets-datacollector-groovy_2_4-lib streamsets-datacollector-jdbc-lib streamsets-datacollector-jms-lib streamsets-datacollector-jython_2_7-lib streamsets-datacollector-stats-lib streamsets-datacollector-windows-lib"
 
+# A space separated list of enterprise stage libs to download
 SDC_ENTERPRISE_STAGE_LIBS="streamsets-datacollector-databricks-lib-1.0.0 streamsets-datacollector-snowflake-lib-1.4.0 streamsets-datacollector-oracle-lib-1.2.0 streamsets-datacollector-sql-server-bdc-lib-1.0.1"
 
 # tmp dir to unpack the stage libs
